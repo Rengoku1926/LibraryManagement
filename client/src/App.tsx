7e9a864c925e2e149b7691f95a8888ccb0576dfb
Prateek <prateekmahapatra1926@gmail.com>
2024-09-30 02:52:16 +0530

    unused corrected

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,10 @@
 import { useState } from 'react'
 
 import './App.css'
-import AddUser from './Components/User/AddUser'
-import GetAllUsers from './Components/User/GetAllUsers'
-import UserDashboard from './Components/User/UserDashboard'
 import MainDashboard from './Components/Dashboard/MainDashboard'
-import AddBook from './Components/Book/AddBook'
-import IssueBook from './Components/Transaction/IssueBook'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
